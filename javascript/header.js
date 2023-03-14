cefbf483ba8ec43660d7595a12d1f9b2a1298425
Binhle <binh.le@quano6.com>
2023-03-14 17:15:17 +0700

    finishing home page

--- javascript/header.js
+++ javascript/header.js
@@ -1,5 +1,5 @@
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 300) {
+  if (document.documentElement.scrollTop > 150) {
     document.getElementById("page-header-container").classList.add("header-sticky");
   } else {
     document.getElementById("page-header-container").classList.remove("header-sticky");
