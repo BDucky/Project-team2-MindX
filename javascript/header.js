304bf7d3aac6aedf3d67819d6c2091208c016694
Binhle <binh.le@quano6.com>
2023-03-25 17:11:31 +0700

    finishing project

--- javascript/header.js
+++ javascript/header.js
@@ -1,8 +1,20 @@
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 150) {
     document.getElementById("page-header-container").classList.add("header-sticky");
+    const openElements = document.querySelectorAll(".open");
+    openElements.forEach((element) => {
+      element.classList.remove("open");
+    });
   } else {
     document.getElementById("page-header-container").classList.remove("header-sticky");
   }
 };
 
+document.getElementsByClassName('animated-header')[0]
+  .addEventListener('click', function (event) {
+    document.getElementsByClassName('animated-header')[0].classList.toggle('open');
+    document.getElementsByClassName('page-header')[0].classList.toggle('open');
+    document.getElementsByClassName('page-header-subtitle')[0].classList.toggle('open');
+    document.getElementsByClassName('page-header-container')[0].classList.toggle('open');
+    document.getElementsByTagName('body')[0].style.overflow = "none";
+  });
